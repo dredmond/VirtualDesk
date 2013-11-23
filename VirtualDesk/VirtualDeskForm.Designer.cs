af38123095ba301f2c00f496f94f739229bbe589
Donny Redmond <dredmond@cloud816.com>
2013-11-22 22:33:29 -0600

    Added Files to the Repository.

---
+++ VirtualDesk/VirtualDeskForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace VirtualDesk
+{
+    partial class VirtualDeskForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.desktop1 = new System.Windows.Forms.PictureBox();
+            this.desktop2 = new System.Windows.Forms.PictureBox();
+            this.desktop3 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.desktop1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.desktop2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.desktop3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // desktop1
+            // 
+            this.desktop1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.desktop1.Location = new System.Drawing.Point(12, 12);
+            this.desktop1.Name = "desktop1";
+            this.desktop1.Size = new System.Drawing.Size(104, 80);
+            this.desktop1.TabIndex = 0;
+            this.desktop1.TabStop = false;
+            this.desktop1.Click += new System.EventHandler(this.desktop1_Click);
+            // 
+            // desktop2
+            // 
+            this.desktop2.BackColor = System.Drawing.Color.Lime;
+            this.desktop2.Location = new System.Drawing.Point(122, 12);
+            this.desktop2.Name = "desktop2";
+            this.desktop2.Size = new System.Drawing.Size(104, 80);
+            this.desktop2.TabIndex = 1;
+            this.desktop2.TabStop = false;
+            this.desktop2.Click += new System.EventHandler(this.desktop2_Click);
+            // 
+            // desktop3
+            // 
+            this.desktop3.Location = new System.Drawing.Point(232, 12);
+            this.desktop3.Name = "desktop3";
+            this.desktop3.Size = new System.Drawing.Size(104, 80);
+            this.desktop3.TabIndex = 2;
+            this.desktop3.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 98);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(324, 117);
+            this.textBox1.TabIndex = 3;
+            // 
+            // VirtualDeskForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 227);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.desktop3);
+            this.Controls.Add(this.desktop2);
+            this.Controls.Add(this.desktop1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "VirtualDeskForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Virtual Desk";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.desktop1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.desktop2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.desktop3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox desktop1;
+        private System.Windows.Forms.PictureBox desktop2;
+        private System.Windows.Forms.PictureBox desktop3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ VirtualDesk/VirtualDeskForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace VirtualDesk
+{
+    public partial class VirtualDeskForm : Form
+    {
+        private readonly int _initialX;
+        private readonly int _initialY;
+        private IntPtr _desktopPointer = IntPtr.Zero;
+        private IntPtr _mainDesktop = IntPtr.Zero;
+
+        /*
+         * DESKTOP_CREATEMENU (0x0004L)	Required to create a menu on the desktop.
+DESKTOP_CREATEWINDOW (0x0002L)	Required to create a window on the desktop.
+DESKTOP_ENUMERATE (0x0040L)	Required for the desktop to be enumerated.
+DESKTOP_HOOKCONTROL (0x0008L)	Required to establish any of the window hooks.
+DESKTOP_JOURNALPLAYBACK (0x0020L)	Required to perform journal playback on a desktop.
+DESKTOP_JOURNALRECORD (0x0010L)	Required to perform journal recording on a desktop.
+DESKTOP_READOBJECTS (0x0001L)	Required to read objects on the desktop.
+DESKTOP_SWITCHDESKTOP (0x0100L)	Required to activate the desktop using the SwitchDesktop function.
+DESKTOP_WRITEOBJECTS (0x0080L)	Required to write objects on the desktop.*/
+        
+        [Flags]
+        enum AccessRights : uint
+        {
+            DESKTOP_CREATEMENU = 0x0004,
+            DESKTOP_CREATEWINDOW = 0x0002,
+            DESKTOP_ENUMERATE = 0x0040,
+            DESKTOP_HOOKCONTROL = 0x0008,
+            DESKTOP_JOURNALPLAYBACK = 0x0020,
+            DESKTOP_JOURNALRECORD = 0x0010,
+            DESKTOP_READOBJECTS  = 0x0001,
+            DESKTOP_SWITCHDESKTOP = 0x0100,
+            DESKTOP_WRITEOBJECTS = 0x0080,
+            GENERIC_ALL = DESKTOP_CREATEMENU | DESKTOP_CREATEWINDOW | DESKTOP_ENUMERATE 
+                | DESKTOP_HOOKCONTROL | DESKTOP_JOURNALPLAYBACK | DESKTOP_JOURNALRECORD
+                | DESKTOP_READOBJECTS | DESKTOP_SWITCHDESKTOP | DESKTOP_WRITEOBJECTS
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr CreateDesktop(string lpszDesktop, IntPtr lpszDevice, IntPtr pDevmode, int dwFlags,
+            uint dwDesiredAccess, IntPtr lpsa);
+
+        [DllImport("user32.dll")]
+        public static extern bool CloseDesktop(IntPtr hDesktop);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetThreadDesktop(int dwThreadId);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetThreadDesktop(IntPtr hDesktop);
+
+        [DllImport("user32.dll")]
+        public static extern bool SwitchDesktop(IntPtr hDesktop);
+
+        [DllImport("Kernel32.dll")]
+        public static extern int GetCurrentThreadId();
+
+        /*
+         * HDESK WINAPI GetThreadDesktop(
+              _In_  DWORD dwThreadId
+            );
+         * 
+         * BOOL WINAPI SetThreadDesktop(
+  _In_  HDESK hDesktop
+);
+         * 
+         * BOOL WINAPI SwitchDesktop(
+  _In_  HDESK hDesktop
+);
+         */
+
+        public VirtualDeskForm()
+        {
+            InitializeComponent();
+            
+            _initialY = Screen.PrimaryScreen.WorkingArea.Height - Height;
+            _initialX = Screen.PrimaryScreen.WorkingArea.Width - Width;
+
+            Location = new Point(_initialX, _initialY);
+            Move += VirtualDeskForm_Move;
+            FormClosing += VirtualDeskForm_FormClosing;
+
+            _desktopPointer = CreateDesktop("NewDesktop", IntPtr.Zero, IntPtr.Zero, 0, (uint)AccessRights.GENERIC_ALL, IntPtr.Zero);
+            _mainDesktop = GetThreadDesktop(GetCurrentThreadId());
+
+            textBox1.AppendText("Desktop Pointer: " + _desktopPointer + "\r\n");
+            textBox1.AppendText("Main Desktop Pointer: " + _mainDesktop + "\r\n");
+        }
+
+        void VirtualDeskForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            desktop1_Click(null, null);
+
+            if (_desktopPointer != IntPtr.Zero)
+            {
+                if (!CloseDesktop(_desktopPointer))
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        void VirtualDeskForm_Move(object sender, EventArgs e)
+        {
+            Location = new Point(_initialX, _initialY);
+        }
+
+        private void desktop2_Click(object sender, EventArgs e)
+        {
+            if (SetThreadDesktop(_desktopPointer))
+            {
+                textBox1.AppendText("SetThread Success!\r\n");
+            }
+            else
+            {
+                textBox1.AppendText("SetThreadFailed Failed\r\n");
+            }
+
+            if (SwitchDesktop(_desktopPointer))
+            {
+                textBox1.AppendText("Switch Success!\r\n");
+            }
+            else
+            {
+                textBox1.AppendText("Switch Failed\r\n");
+            }
+        }
+
+        private void desktop1_Click(object sender, EventArgs e)
+        {
+            if (SetThreadDesktop(_mainDesktop))
+            {
+                textBox1.AppendText("SetThread Success!\r\n");
+            }
+            else
+            {
+                textBox1.AppendText("SetThreadFailed Failed\r\n");
+            }
+
+            if (SwitchDesktop(_mainDesktop))
+            {
+                textBox1.AppendText("Switch Success!\r\n");
+            }
+            else
+            {
+                textBox1.AppendText("Switch Failed\r\n");
+            }
+        }
+    }
+}
