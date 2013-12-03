ca2bea960bcbe84edcc670303a5ea26e0a215557
Donny Redmond <dredmond@cloud816.com>
2013-12-03 12:59:54 -0600

    Added a Simulated VirtualDesk Project since the original won't work as I had planned.

---
+++ VirtualDesk-Simulated/Desktop/General.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirtualDesk_Simulated.Desktop
+{
+    public static class General
+    {
+        /*
+private void button1_Click(object sender, EventArgs e)
+{
+    Size sz = Screen.PrimaryScreen.Bounds.Size;
+    IntPtr hDesk = GetDesktopWindow();
+    IntPtr hSrce = GetWindowDC(hDesk);
+    IntPtr hDest = CreateCompatibleDC(hSrce);
+    IntPtr hBmp = CreateCompatibleBitmap(hSrce, sz.Width, sz.Height);
+    IntPtr hOldBmp = SelectObject(hDest, hBmp);
+    bool b = BitBlt(hDest, 0, 0, sz.Width, sz.Height, hSrce, 0, 0, CopyPixelOperation.SourceCopy | CopyPixelOperation.CaptureBlt);
+    Bitmap bmp = Bitmap.FromHbitmap(hBmp);
+    SelectObject(hDest, hOldBmp);
+    DeleteObject(hBmp);
+    DeleteDC(hDest);
+    ReleaseDC(hDesk, hSrce);
+    bmp.Save(@"c:\temp\test.png");
+    bmp.Dispose();
+}*/
+
+        public static Image GetSnapshot()
+        {
+            var sz = Screen.PrimaryScreen.Bounds.Size;
+            var hDesk = WinApi.GetDesktopWindow();
+            var hSrc = WinApi.GetWindowDC(hDesk);
+            var hDest = WinApi.CreateCompatibleDC(hSrc);
+            var hBmp = WinApi.CreateCompatibleBitmap(hSrc, sz.Width, sz.Height);
+            var hOldBmp = WinApi.SelectObject(hDest, hBmp);
+
+            WinApi.BitBlt(hDest, 0, 0, sz.Width, sz.Height, hSrc, 0, 0, CopyPixelOperation.SourceCopy | CopyPixelOperation.CaptureBlt);
+            
+            var bmp = Image.FromHbitmap(hBmp);
+            WinApi.SelectObject(hDest, hOldBmp);
+            WinApi.DeleteObject(hBmp);
+            WinApi.DeleteDC(hDest);
+            WinApi.ReleaseDC(hDesk, hSrc);
+
+            return bmp;
+        }
+    }
+}
---
+++ VirtualDesk-Simulated/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VirtualDesk_Simulated.Desktop;
+
+namespace VirtualDesk_Simulated
+{
+    public partial class VirtualDeskForm : Form
+    {
+        public VirtualDeskForm()
+        {
+            InitializeComponent();
+
+            Shown += VirtualDeskForm_Shown;
+            Move += VirtualDeskForm_Move;
+            timer1.Tick += timer1_Tick;
+        }
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateSelectedDesktopScreen();
+        }
+
+        void VirtualDeskForm_Move(object sender, EventArgs e)
+        {
+            //MoveToBottomRight();
+            timer1.Enabled = true;
+        }
+
+        void VirtualDeskForm_Shown(object sender, EventArgs e)
+        {
+            MoveToBottomRight();
+        }
+
+        void MoveToBottomRight()
+        {
+            var sh = Screen.PrimaryScreen.WorkingArea.Height;
+            var sw = Screen.PrimaryScreen.WorkingArea.Width;
+
+            Location = new Point(sw - Width, sh - Height);
+        }
+
+        void UpdateSelectedDesktopScreen()
+        {
+            var snapshot = General.GetSnapshot();
+            pictureBox1.Image = snapshot;
+            pictureBox2.Image = snapshot;
+            pictureBox3.Image = snapshot;
+            pictureBox4.Image = snapshot;
+        }
+    }
+}
